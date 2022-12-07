922799c3ec893ea8cee3f8a61b1c15fa53567d18
Leonardo Papandrea <papandrealeonardo@hotmail.com>
2022-12-07 16:58:49 -0300

    actualizado

--- js/script.js
+++ js/script.js
@@ -279,33 +279,4 @@ ultima.addEventListener('click', () => {
     }
 })
 
-const criptoYa = "https://criptoya.com/api/dolar";
-
-const dolar = document.createElement("section");
-
-setInterval(() => {
-    fetch(criptoYa)
-        .then(response => response.json())
-        .then(({
-            blue,
-            ccb,
-            ccl,
-            mep,
-            oficial,
-            solidario
-        }) => {
-            dolar.innerHTML = `
-            <h2>Tipos de Dolar: </h2>
-            <p>Dolar Oficial: ${oficial} </p>
-            <p>Dolar Solidario: ${solidario} </p>
-            <p>Dolar MEP: ${mep} </p>
-            <p>Dolar CCL: ${ccl} </p>
-            <p>Dolar Ccb: ${ccb} </p>
-            <p>Dolar Blue: ${blue} </p>
-            `
-        })
-        .catch(error => console.error(error))
-}, 3000)
-
-div.append(dolar);
 
